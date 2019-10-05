f67b1fafd0a7e54a0cdd3df6999774a4bf604dad
Pet User <pet@email.com>
2019-10-06 00:04:17 +0200

    Removed override

--- spring5-demo-one/src/main/java/com/webcache/springdemo/HappyFortuneService.java
+++ spring5-demo-one/src/main/java/com/webcache/springdemo/HappyFortuneService.java
@@ -2,7 +2,6 @@ package com.webcache.springdemo;
 
 public class HappyFortuneService implements IFortuneService {
 
-	@Override
 	public String getFortune() {
 		return "Today is your lukcy day";
 	}
